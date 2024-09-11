bffb70a030ae0ed0ae67e89ce022cff99b123e1f
kelvinMulinge <kelvinmulinge9702@gmail.com>
2024-09-11 15:34:16 +0300

    What is a test

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const name = "Joe";
 const height = 74;
 const message = `${name} is ${height} inches tall`;
+console.log(message);
+const Name ="Susan";
+const Height = "39";
+const Message =`${Name} is ${Height} inches tall`;
+console.log(Message);
 
 module.exports = {name, height, message};
